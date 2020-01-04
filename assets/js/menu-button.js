f945b5b34af3565c94511a16f1fee3316e190ecf
devNicolasNmk <devnicolasnmk@gmail.com>
2020-01-04 18:43:15 +0100

    menu button js correction visibilité

--- assets/js/menu-button.js
+++ assets/js/menu-button.js
@@ -2,11 +2,11 @@ var button_menu = document.getElementById('menu-button');
 var header_ul = document.getElementById('header-ul');
 
 button_menu.addEventListener('click', function () {
-    if (header_ul.style.display == 'none') {
-        header_ul.style.display = 'block';
+    if (header_ul.style.visibility == 'visible') {
+        header_ul.style.visibility = 'hidden';
         //hide    
     } else {
-        header_ul.style.display = 'none'
+        header_ul.style.visibility = 'visible';
     }
 });
 
